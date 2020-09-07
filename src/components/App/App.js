6fe03da70c5d2af4670f7e30df70d3b924475c60
Pavel Stashchenko <pavel.stashchenko@gmail.com>
2020-09-07 14:35:32 +0300

    Add additional packages to package.json and Updata App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+import CityInput from '../CityInput/CityInput';
+
+
+const HeadH1 = styled.h1`
+    width: 25%;
+    margin: 25px auto;
+    padding: 15px;
+    text-align: center;
+    border: 1px solid black;
+    border-radius: 1em;
+`;
 
 export class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            city: []
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1>Start app</h1>
+                <HeadH1>Weather App</HeadH1>
+                <CityInput />
             </div>
         )
     }
